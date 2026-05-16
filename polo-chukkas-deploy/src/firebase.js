87fdca69b2514b11beea8b458656add4f91a6af1
andyboudier <andyboudier@googlemail.com>
2026-05-16 19:52:38 +0100

    Update firebase.js

--- polo-chukkas-deploy/src/firebase.js
+++ polo-chukkas-deploy/src/firebase.js
@@ -1,13 +1,14 @@
-// Paste the config from the Firebase console here.
-// Console → Project Settings → General → "Your apps" → SDK setup → "Config"
-//
-// It will look like this:
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBEPZpBeZLmUQdtzGCY7UCIwnGzP8f1xpQ",
-  authDomain: "tedworth-park-polo.firebaseapp.com",
-  projectId: "tedworth-park-polo",
-  storageBucket: "tedworth-park-polo.firebasestorage.app",
-  messagingSenderId: "856516284253",
-  appId: "1:856516284253:web:68b21c3b23dd8d504062b4"
+export const firebaseConfig = {
+    apiKey: "AIzaSyBEPZpBeZLmUQdtzGCY7UCIwnGzP8f1xpQ",
+    authDomain: "tedworth-park-polo.firebaseapp.com",
+    projectId: "tedworth-park-polo",
+    storageBucket: "tedworth-park-polo.firebasestorage.app",
+    messagingSenderId: "856516284253",
+    appId: "1:856516284253:web:68b21c3b23dd8d504062b4"
 };
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
